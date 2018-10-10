6a55b8b0941810719ed6d812cddf5bf97f7fc8ed
ChenJason <Jason@csl1997.local>
2018-10-10 15:14:31 -0400

    Commit my Lab5.

--- Lab5/bitmap_printer.c
+++ Lab5/bitmap_printer.c
@@ -38,6 +38,12 @@ int main(int argc, char **argv) {
     }
 
     // Clean up: you need to do this!
+    fclose(image);
 
+    for (size_t i = 0; i < height; i++) {
+      free(pixels[i]);
+    }
+    free(pixels);
+    
     return 0;
 }
