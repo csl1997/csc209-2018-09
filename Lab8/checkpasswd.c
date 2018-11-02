75f28b46676adac981dea9137123c70ec1347de7
ChenJason <Jason@csl1997.local>
2018-11-01 23:03:18 -0400

    commit Lab8.

--- Lab8/checkpasswd.c
+++ Lab8/checkpasswd.c
@@ -15,6 +15,7 @@
 int main(void) {
   char user_id[MAXLINE];
   char password[MAXLINE];
+  int fd[2], status, n;
 
   if(fgets(user_id, MAXLINE, stdin) == NULL) {
       perror("fgets");
@@ -24,7 +25,71 @@ int main(void) {
       perror("fgets");
       exit(1);
   }
-  
+
+  if (pipe(fd) == -1) {
+    perror("pipe");
+    exit(1);
+  }
+
+  int r = fork();
+
+  if (r < 0) {
+    perror("fork");
+    exit(1);
+
+  } else if (r == 0) { // child
+
+    if ((dup2(fd[0], fileno(stdin))) == -1) {
+      perror("dup2");
+      exit(1);
+    }
+
+    if ((close(fd[0])) == -1) {
+      perror("close");
+    }
+
+    if ((close(fd[1])) == -1) {
+      perror("close");
+    }
+
+    execl("./validate", "validate", NULL);
+    fprintf(stderr, "ERROR: exec should not return\n");
+
+  } else { // parent
+
+      if ((n = write(fd[1], user_id, MAX_PASSWORD)) == -1) {
+        perror("write");
+        exit(1);
+      }
+
+      if ((n = write(fd[1], password, MAX_PASSWORD)) == -1) {
+        perror("write");
+        exit(1);
+      }
+
+      if ((close(fd[1])) == -1) {
+        perror("close");
+      }
+
+      if ((close(fd[0])) == -1) {
+        perror("close");
+      }
+
+      wait(&status);
+
+      if (WIFEXITED(status)) {
+        int exit_val = WEXITSTATUS(status);
+        if (exit_val == 0) {
+          printf("%s", SUCCESS);
+        } else if (exit_val == 2) {
+          printf("%s", INVALID);
+        } else if (exit_val == 3) {
+          printf("%s", NO_USER);
+        } else {
+          perror("exit value");;
+        }
+      }
+  }
 
 
   return 0;
